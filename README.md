e2f23947db38d2a036ab2b2926d7d4a18c4e4bbd
Naomi Johnson <naomikaiena@gmail.com>
2018-12-08 10:01:13 -0500

    edits with Readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TriviaGame
+
+BEYONCE is a pop/hiphop/rnb Icon. It is only right that to be apart of the "Bey Hive" you have to know all about her. Test your knowledge in this Beyonce Trivia game. The rules are simple;
+
+There are Five questions pertaining to the events in Beyonce's life.
+
+You have four options, click the correct one and a screen comes up telling you that you are correct, click the wrong answer and you are then told that you are no correct.
+
+On the final screen you will see how many you got correct and incorrect and have the option to play again.
+
+Enjoy!!!
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $("#remaining-time").hide();
+  $(".answers").hide();
   $("#start").on("click", startGame);
   // $(document).on('click' , '.option', guessChecker);
-  // $("#firstAnswer").hide();
-  
-  
+  // $("#firstAnswer").hide(); 
 });
 
-var correctAnswers =  0;
+var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
 var currentSet = 0;
-var timer = 20;
-// var timerTwo = 5;
+var timer = 21;
+var timerTwo = 5;
 var timerOn = false;
 var timerId = "";
+var timerStart = "";
+
 
 var beyonce = [{
   question: "What year was Beyonce born?",
@@ -23,23 +24,24 @@ var beyonce = [{
   option3: "1982",
   option4: "1985",
   answer: "1981",
-  // img: "...img",
+  img: "assets/images/BeyonceBaby.gif",
+
 },
 {
   question: "Where was Beyonce born?",
   option1: "Dallas, Tx",
   option2: "Houston, Tx",
-  option3: "Atlanta, Ga", 
-  option4: "New York, Ny", 
+  option3: "Atlanta, Ga",
+  option4: "New York, Ny",
   answer: "Houston, Tx",
-  // img: "...img",
+  img: "",
 },
 {
   question: "When did Beyonce go solo?",
   option1: "2002",
   option2: "2003",
-  option3: "2006", 
-  option4: "2005", 
+  option3: "2006",
+  option4: "2005",
   answer: "2003",
   img: "...img",
 },
@@ -47,8 +49,8 @@ var beyonce = [{
   question: "What's the name of Beyonce's second solo album?",
   option1: "Dangerously in Love",
   option2: "Formation",
-  option3: "Beyonce", 
-  option4: "Sasha Fierce", 
+  option3: "Beyonce",
+  option4: "Sasha Fierce",
   answer: "Beyonce",
   img: "...img",
 },
@@ -56,241 +58,256 @@ var beyonce = [{
   question: "How many SOLO albums has Beyonce released?",
   option1: "3",
   option2: "6",
-  option3: "5", 
-  option4: "7", 
+  option3: "5",
+  option4: "7",
   answer: "7",
   img: "...img",
 },]
 
-// var beyonce = {
-//   questions: {
-//     q1: "What year was Beyonce born?",
-//     q2: "Where was Beyonce born?",
-//     q3: "When did Beyonce go solo?",
-//     q4: "What's the name of Beyonce's second solo album?",
-//     q5: "How many SOLO albums has Beyonce released?",
-//     q6: "What are the names of Beyonce's twins?",
-//     q7: "When did Beyonce get married?"
-//   },
-//   options: {
-//     q1: ["1981", "1980", "1982", "1985"],
-//     q2: ["Dallas, Tx", "Houston, Tx", "Atlanta, Ga", "New York, Ny"],
-//     q3: ["2002", "2003", "2006", "2005"],
-//     q4: ["Dangerously in Love", "Formation", "Beyonce", "Sasha Fierce"],
-//     q5: ["3", "6","5", "7" ],
-//     q6: ["Rumi & Sir", "Romi & Sor", "Rami & Sur", "Green & Purple"],
-//     q7: ["January 4, 2009", "April 4, 2008", "March 4, 2010","September 4, 2008"]
-//   },
-//   answers: {
-//     q1: "1981",
-//     q2: "Houston, TX",
-//     q3: "2003",
-//     q4: "Beyonce",
-//     q5: "7",
-//     q6: "Rumi & Sir",
-//     q7: "April 4, 2008"
-//   }};
-  
-  
-  // method to loop through and display questions and options 
-// function thankYouNext(){
-//   $('#options').hide();
-//   $("#results").html("Correct the answer was " + currentAnswer  );
-//   $("#timer").hide();
-//   $("#remaining-time").hide();
-//   $("#question").hide();
-//   timerTwo = 5;
-//   $('#timerTwo').text(timerTwo);
-//   // nextQuestion();
-//   timerTwo();
 
-// } 
-function startGame(){
-    currentSet = 0;
-    correctAnswers = 0;
-    incorrectAnswers = 0;
-    unanswered = 0;
-    // clearInterval(timerId);
-    // $('#game').show();
-    // $('#results').html('');
-    // $('#timer').text(timer);
-    // $('#timerTwo').text(timerTwo);
-    $('#start').hide();
-    
-    // $('#remaining-time').show();
-    nextQuestion();
-  };
-// function timerTwo(){
-//   timerTwo = 5;
-//   $('#timerTwo').text(timerTwo);
-//   $("#timerTwo").show();
-//   timerId = setInterval(timerRunning, 1000);
-//   if (timerTwo > -1 && currentSet < Object.keys(beyonce.questions).length){
-//     $('#timerTwo').text(timerTwo);
-//     timerTwo--;}
-//   nextQuestion();
-  
-// }
- function nextQuestion(){
+function startGame() {
+  currentSet = 0;
+  correctAnswers = 0;
+  incorrectAnswers = 0;
+  unanswered = 0;
+  // clearInterval(timerId);
+  // $('#game').show();
+  // $('#results').html('');
+  // $('#timer').text(timer);
+  // $('#timerTwo').text(timerTwo);
+  $('#start').hide();
+
+  // $('#remaining-time').show();
+  nextQuestion();
+};
+// need to stu[]
+function nextQuestion() {
   $(".answers").show();
-   clearInterval(timerId);
-    timer = 21;
-    //  $('#timer').removeClass('last-seconds');
-    // $("#results").hide();
-    // $("#timer").show();
-    
-    newScreen();
-    // function newScreen(){
-    // $("#remaining-time").show();
-    // $("#myQuestions").text(beyonce[i].question);
-    // $(".answer1").text(beyonce[i].option1);
-    // $(".answer2").text(beyonce[i].option2);
-    // $(".answer3").text(beyonce[i].option3);
-    // $(".answer4").text(beyonce[i].option4);
-    // }
+  clearInterval(timerId);
+  timer = 21;
+  //  $('#timer').removeClass('last-seconds');
+  // $("#results").hide();
+  // $("#timer").show();
 
-    // $('#timer').text(timer);
-    // timerId = setInterval(timerRunning, 1000);
-    // var questionContent = Object.values(beyonce.questions)[currentSet];
-    // $('#question').text(questionContent);
-    // var questionOptions = Object.values(beyonce.options)[currentSet];
-    // $.each(questionOptions, function(index, key){
-    
-    // $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+  newScreen();
 
-    // })
-   var countDown = setInterval (timesUp, 1000);
-function timesUp(){
-  timer --;
-  $("#timer").text(timer);
+  //    var countDown = setInterval (timesUp, 1000);
+  // timesUp();
+  // function timesUp(){
+  //   timer --;
+
+  //   $("#timer").text(timer);
+  //   if (timer === 0){
+  //     clearInterval(countDown);
+  //     i++;
+  //     newScreen();
+  //     unanswered++;
+
+  //   }
+  // }
+
+};
+function yayYou() {
+  // timer = 5;
+  // clearInterval(countDown);
+  $("#myQuestions").hide();
+  $(".answers").hide();
+  $("#remaining-time").hide();
+  $("#answersShown").text("Yay you got it! The correct answer was " + (beyonce[i].answer));
+  $("#image").attr("src", beyonce[i].img);
+  // if (timer <== 0){
+  newScreen();
 }
-    
-  };
+function booYou() {
+  // timer = 5;
+  // clearInterval(countDown);
+  $("#myQuestions").hide();
+  $(".answers").hide();
+  $("#remaining-time").hide();
+  $("#answersShown").text("So sorry the correct answer was " + (beyonce[i].answer));
+  $("#image").attr("src", beyonce[i].img);
+  // if (timer <== 0){
+  newScreen();
+}
 
-  function newScreen(){
-    if(i < beyonce.length){
-    $("#remaining-time").show();
-    $("#myQuestions").text(beyonce[i].question);
-    $(".answer1").text(beyonce[i].option1);
-    $(".answer2").text(beyonce[i].option2);
-    $(".answer3").text(beyonce[i].option3);
-    $(".answer4").text(beyonce[i].option4);
+var countDown = setInterval(timesUp, 1000);
+
+
+function timesUp() {
+  timer--;
+
+  $("#timer").text(timer);
+  if (timer === 0) {
+    clearInterval(countDown);
+    i++;
+    booYou();
+    unanswered++;
+
+  }
+}
+  function newScreen() {
+    timer = 21;
+    timer--;
+    $("#timer").text(timer);
+    // timesUp();
+    // startInterval(reStart);
+
+    if (i < beyonce.length) {
+      $("#remaining-time").show();
+      $("#myQuestions").text(beyonce[i].question);
+      $(".answer1").text(beyonce[i].option1);
+      $(".answer2").text(beyonce[i].option2);
+      $(".answer3").text(beyonce[i].option3);
+      $(".answer4").text(beyonce[i].option4);
+      // $("#firstAnswer").
+      // if (timer === 0){
+      //   clearInterval(timerId);
+      //   i++;
+      //   newScreen();
+      //   unanswered
+      // }
     }
-    
-    if (i === beyonce.length){
+
+    if (i === beyonce.length) {
 
       $('#results')
-        .html('<h3>Thank you for playing!</h3>'+
-        '<p>Correct: '+ correctAnswers +'</p>'+
-        '<p>Incorrect: '+ incorrectAnswers +'</p>'+
-        '<p>Unaswered: '+ unanswered +'</p>'+
-        '<p>Please play again!</p>');
-        $("#myQuestions").hide();
-        $(".answers").hide();
-        $("#remaining-time").hide();
-      }
+        .html('<h3>Thank you for playing!</h3>' +
+          '<p>Correct: ' + correctAnswers + '</p>' +
+          '<p>Incorrect: ' + incorrectAnswers + '</p>' +
+          '<p>Unaswered: ' + unanswered + '</p>' +
+          '<p>Please play again!</p>');
+      $("#myQuestions").hide();
+      $(".answers").hide();
+      $("#remaining-time").hide();
+    }
   }
-    
+  // var reStart = startInterval ( timesUp, 1000);  
+  var countDown = setInterval(timesUp, 1000);
+
+
+  // function timesUp() {
+  //   timer--;
+
+  //   $("#timer").text(timer);
+  //   if (timer === 0) {
+  //     clearInterval(countDown);
+  //     i++;
+  //     booYou();
+  //     unanswered++;
+
+  //   }
+  // }
+
+
+
+
+
+
+
+
   var i = 0;
-  $(document).on("click", ".answers", function(){ 
-    if($(this).text() === beyonce[i].answer){
-    // alert("test");
+  $(document).on("click", ".answers", function () {
     timer = 21;
-    console.log(this)
-    // i++;
-    correctAnswers++;
-    i++;
-    newScreen();
-    
-    } else{ 
+    if ($(this).text() === beyonce[i].answer) {
+      // alert("test");
+      timer = 21;
+      console.log(this)
+      // i++;
+      correctAnswers++;
+      i++;
+      yayYou();
+      // newScreen();
+
+    } else {
       incorrectAnswers++;
       console.log(this)
       timer = 21;
       i++;
+      booYou();
       newScreen();
-     
+
     }
-  
+
   })
-//   function timerRunning(){
-//     if(timer > -1 && currentSet < Object.keys(beyonce.questions).length){
-//       $('#timer').text(timer);
-//       timer--;
-//     }
-//     else if(timer === -1){
-//       unanswered++;
-//       result = false;
-//       clearInterval(timerId);
-//       resultId = setTimeout(guessResult, 1000);
-//       $("#timer").hide(timer);
-//       $("#remaining-time").hide();
-//       $("#question").hide();
-//       $('#options').hide();
-//       // alert("you got it");
-//       $("#results").html("Out of time! The answer was " + Object.values(beyonce.answers)[currentSet] );
-//       nextQuestion();
-      
-//       // $("#results").html('<h3>Out of time! The answer was '+ Object.values(beyonce.answers)[currentSet] +'</h3>');
-//     }
-//     // if all the questions have been shown end the game, show results
-//     else if(currentSet === Object.keys(beyonce.questions).length){
-      
-      // adds results of game (correct, incorrect, unanswered) to the page
-    // if (i = beyonce.length){
+  //   function timerRunning(){
+  //     if(timer > -1 && currentSet < Object.keys(beyonce.questions).length){
+  //       $('#timer').text(timer);
+  //       timer--;
+  //     }
+  //     else if(timer === -1){
+  //       unanswered++;
+  //       result = false;
+  //       clearInterval(timerId);
+  //       resultId = setTimeout(guessResult, 1000);
+  //       $("#timer").hide(timer);
+  //       $("#remaining-time").hide();
+  //       $("#question").hide();
+  //       $('#options').hide();
+  //       // alert("you got it");
+  //       $("#results").html("Out of time! The answer was " + Object.values(beyonce.answers)[currentSet] );
+  //       nextQuestion();
 
-    //   $('#results')
-    //     .html('<h3>Thank you for playing!</h3>'+
-    //     '<p>Correct: '+ correctAnswers +'</p>'+
-    //     '<p>Incorrect: '+ incorrectAnswers +'</p>'+
-    //     '<p>Unaswered: '+ unanswered +'</p>'+
-    //     '<p>Please play again!</p>');
-    //   }
-//       // hide game sction
-//       $('#game').hide();
-      
-//       // show start button to begin a new game
-//       $('#start').show();
-//     }
-    
-//   };
-//  var currentAnswer = Object.values(beyonce.answers)[currentSet];
-//   function guessChecker() {
-//     // var resultId;
-   
-//     if($(this).text() === currentAnswer){
-//       $(this).addClass('btn-success').removeClass('btn-info');
-//       correctAnswers++;
-//       clearInterval(timerId);
-//       resultId = setTimeout(guessResult, 1000);
-//       thankYouNext();
-      
-//     }
-  
-//     // else the user picked the wrong option, increment incorrect
-//     else if($(this).text() !== currentAnswer) {
-//       // turn button clicked red for incorrect
-//       $(this).addClass('btn-danger').removeClass('btn-info');
-      
-//       incorrectAnswers++;
-//       clearInterval(timerId);
-//       resultId = setTimeout(guessResult, 1000);
-//       // $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
-//       $("#timer").hide(timer);
-//       $("#remaining-time").hide();
-//       $("#question").hide();
-//       $('#options').hide();
-//       // alert("you got it");
-//       $("#results").html("Maybe next time...The answer was " + currentAnswer );
-//       nextQuestion();
-//     }
-  
+  //       // $("#results").html('<h3>Out of time! The answer was '+ Object.values(beyonce.answers)[currentSet] +'</h3>');
+  //     }
+  //     // if all the questions have been shown end the game, show results
+  //     else if(currentSet === Object.keys(beyonce.questions).length){
 
-// };
+  // adds results of game (correct, incorrect, unanswered) to the page
+  // if (i = beyonce.length){
 
- 
-  function guessResult(){
+  //   $('#results')
+  //     .html('<h3>Thank you for playing!</h3>'+
+  //     '<p>Correct: '+ correctAnswers +'</p>'+
+  //     '<p>Incorrect: '+ incorrectAnswers +'</p>'+
+  //     '<p>Unaswered: '+ unanswered +'</p>'+
+  //     '<p>Please play again!</p>');
+  //   }
+  //       // hide game sction
+  //       $('#game').hide();
+
+  //       // show start button to begin a new game
+  //       $('#start').show();
+  //     }
+
+  //   };
+  //  var currentAnswer = Object.values(beyonce.answers)[currentSet];
+  //   function guessChecker() {
+  //     // var resultId;
+
+  //     if($(this).text() === currentAnswer){
+  //       $(this).addClass('btn-success').removeClass('btn-info');
+  //       correctAnswers++;
+  //       clearInterval(timerId);
+  //       resultId = setTimeout(guessResult, 1000);
+  //       thankYouNext();
+
+  //     }
+
+  //     // else the user picked the wrong option, increment incorrect
+  //     else if($(this).text() !== currentAnswer) {
+  //       // turn button clicked red for incorrect
+  //       $(this).addClass('btn-danger').removeClass('btn-info');
+
+  //       incorrectAnswers++;
+  //       clearInterval(timerId);
+  //       resultId = setTimeout(guessResult, 1000);
+  //       // $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+  //       $("#timer").hide(timer);
+  //       $("#remaining-time").hide();
+  //       $("#question").hide();
+  //       $('#options').hide();
+  //       // alert("you got it");
+  //       $("#results").html("Maybe next time...The answer was " + currentAnswer );
+  //       nextQuestion();
+  //     }
+
+
+  // };
+
+
+  function guessResult() {
     currentSet++;
     $('.option').remove();
     $('#results h3').remove();
     nextQuestion();
-}
+  }
